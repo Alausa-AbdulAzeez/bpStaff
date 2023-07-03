00df0d875e2dadde0b23739f8f5ccab242603d1b
Alausa-AbdulAzeez <alausaabdulazeez@gmail.com>
2023-07-03 17:55:29 +0100

    successfully fetched and displayed the companies

--- src/components/viewClientsDatagrid/ViewClientsDatagrid.jsx
+++ src/components/viewClientsDatagrid/ViewClientsDatagrid.jsx
@@ -2,43 +2,33 @@ import {
   Accordion,
   AccordionDetails,
   AccordionSummary,
-  Collapse,
-  List,
   ListItemButton,
   ListItemIcon,
   ListItemText,
-  ListSubheader,
   Typography,
 } from '@mui/material'
 import { Box } from '@mui/system'
 import { DataGrid } from '@mui/x-data-grid'
 import React, { useState } from 'react'
-import { MdCancel, MdExpandLess, MdExpandMore } from 'react-icons/md'
+import { MdCancel } from 'react-icons/md'
 import { FaAngleDown } from 'react-icons/fa'
 import './viewClientsDatagrid.scss'
-import { BsCheck, BsListCheck } from 'react-icons/bs'
+import { BsCheck } from 'react-icons/bs'
 
 const ViewClientsDatagrid = (props) => {
   const [pageSize, setPageSize] = useState(5)
   const [position, setPosition] = useState('-100%')
-  let rows
-  let columns
+
+  // TABLE DATA
+  let rows = props?.tableData
+  console.log(props?.tableData)
   let title
-  const loggedInUserRole = props.userDetails?.role
 
-  // LIST TOGGLE FUNCTIONALITY
-  const [openListItem, setOpenListItem] = React.useState(true)
+  // LOGGED IN USER RLOE
+  const loggedInUserRole = props.userDetails?.data?.role
 
-  const handleClick = () => {
-    setOpenListItem(!openListItem)
-  }
-  // END OF LIST TOGGLE FUNCTIONALITY
-
-  // SET SIDE INFO POSITION
-  const handleSetPosition = () => {
-    setPosition('0')
-  }
-  // END OF SET SIDE INFO POSITION
+  // LOGGED IN USER
+  const userName = props.userDetails?.data?.profile?.fullName
 
   // HANDLE ROW CLICK
   const handleRowClick = (row, e) => {
@@ -56,112 +46,21 @@ const ViewClientsDatagrid = (props) => {
   }
   // END OF HANDLE ROW CLICK
 
-  const receptionistcolumns = [
-    { field: 'compName', headerName: 'Company Name', width: 350 },
+  const columns = [
+    { field: 'clientName', headerName: 'Company Name', width: 350 },
     {
-      field: 'firstName',
-      headerName: 'Number of Candidates',
+      field: 'email',
+      headerName: 'Company Email',
       width: 250,
       editable: false,
     },
     {
-      field: 'Date',
-      headerName: 'Date',
+      field: 'phoneNumber',
+      headerName: 'Company Phone No.',
       width: 350,
       editable: false,
     },
-    // {
-    //   field: 'action',
-    //   headerName: 'Report Status',
-    //   width: 250,
-    //   renderCell: (params) => {
-    //     return (
-    //       <>
-    //         {params.row.attendedTo === 'true' ? (
-    //           <div className='notSent'>Not Sent</div>
-    //         ) : (
-    //           <div className='sent'>Sent</div>
-    //         )}
-    //       </>
-    //     )
-    //   },
-    // },
   ]
-
-  const receptionistRows = [
-    {
-      id: 1,
-      firstName: '1',
-      Date: '1-March-2023',
-      attendedTo: 'false',
-      compName: 'Chicken Republic',
-    },
-    {
-      id: 2,
-
-      firstName: '1',
-      Date: '1-March-2023',
-      attendedTo: 'true',
-      compName: 'Chicken Republic',
-    },
-    {
-      id: 3,
-
-      firstName: '3',
-      Date: '1-March-2023',
-      attendedTo: 'false',
-      compName: 'Chicken Republic',
-    },
-    {
-      id: 4,
-      firstName: '3',
-      Date: '1-March-2023',
-      attendedTo: 'true',
-      compName: 'Chicken Republic',
-    },
-    {
-      id: 5,
-      lastName: 'Targaryen',
-      firstName: '2',
-      Date: '1-March-2023',
-      attendedTo: 'true',
-      compName: 'Chicken Republic',
-    },
-    {
-      id: 6,
-
-      firstName: '2',
-      Date: '1-March-2023',
-      attendedTo: 'true',
-    },
-    {
-      id: 7,
-      compName: 'Chicken Republic',
-      firstName: '3',
-      Date: '1-March-2023',
-      attendedTo: 'true',
-    },
-    {
-      id: 8,
-      firstName: '3',
-      Date: '1-March-2023',
-      attendedTo: 'true',
-      compName: 'Chicken Republic',
-    },
-    {
-      id: 9,
-      firstName: '3',
-      Date: '1-March-2023',
-      attendedTo: 'true',
-      compName: 'Chicken Republic',
-    },
-  ]
-
-  if (loggedInUserRole !== 'null') {
-    rows = receptionistRows
-    columns = receptionistcolumns
-    title = 'Clients'
-  }
 
   return (
     <div className='viewClientsDatagridWraper'>
@@ -222,107 +121,9 @@ const ViewClientsDatagrid = (props) => {
                 <ListItemText primary={`Food Handlers'`} />
                 <ListItemText primary={`Pre Employment`} />
               </ListItemButton>
-              {/* <Typography>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
-                eget.
-              </Typography> */}
             </AccordionDetails>
           </Accordion>
           <br />
-
-          {/* <List
-            sx={{
-              width: '100%',
-              maxWidth: 360,
-              bgcolor: 'background.paper',
-            }}
-            component='nav'
-            aria-labelledby='nested-list-subheader'
-            subheader={
-              <ListSubheader
-                component='div'
-                id='nested-list-subheader'
-                sx={{
-                  fontSize: '16px',
-                  fontWeight: 'bold',
-                  color: 'black',
-                }}
-              >
-                Screening Details
-              </ListSubheader>
-            }
-          >
-            <ListItemButton onClick={handleClick}>
-              <ListItemIcon>
-                <BsListCheck />
-              </ListItemIcon>
-              <ListItemText primary='Pre-Employment' />
-              {openListItem ? <MdExpandLess /> : <MdExpandMore />}
-            </ListItemButton>
-            <Collapse in={openListItem} timeout='auto' unmountOnExit>
-              <List component='div' disablePadding>
-                <ListItemButton sx={{ pl: 4 }}>
-                  <ListItemIcon>
-                    <BsCheck />
-                  </ListItemIcon>
-                  <ListItemText primary='Hepatitis' />
-                </ListItemButton>
-                <ListItemButton sx={{ pl: 4 }}>
-                  <ListItemIcon>
-                    <BsCheck />
-                  </ListItemIcon>
-                  <ListItemText primary='BP' />
-                </ListItemButton>
-              </List>
-            </Collapse>
-          </List>
-          <List
-            sx={{
-              width: '100%',
-              maxWidth: 360,
-              bgcolor: 'background.paper',
-            }}
-            component='nav'
-            aria-labelledby='nested-list-subheader'
-            subheader={
-              <ListSubheader
-                component='div'
-                id='nested-list-subheader'
-                sx={{
-                  fontSize: '16px',
-                  fontWeight: 'bold',
-                  color: 'black',
-                }}
-              >
-                Screening Details
-              </ListSubheader>
-            }
-          >
-            <ListItemButton onClick={handleClick}>
-              <ListItemIcon>
-                <BsListCheck />
-              </ListItemIcon>
-              <ListItemText primary='Pre-Employment' />
-              {openListItem ? <MdExpandLess /> : <MdExpandMore />}
-            </ListItemButton>
-            <Collapse in={openListItem} timeout='auto' unmountOnExit>
-              <List component='div' disablePadding>
-                <ListItemButton sx={{ pl: 4 }}>
-                  <ListItemIcon>
-                    <BsCheck />
-                  </ListItemIcon>
-                  <ListItemText primary='Hepatitis' />
-                </ListItemButton>
-                <ListItemButton sx={{ pl: 4 }}>
-                  <ListItemIcon>
-                    <BsCheck />
-                  </ListItemIcon>
-                  <ListItemText primary='BP' />
-                </ListItemButton>
-              </List>
-            </Collapse>
-          </List> */}
         </div>
 
         <div className='viewClientsDate'>
@@ -335,12 +136,11 @@ const ViewClientsDatagrid = (props) => {
           rows={rows}
           columns={columns}
           pageSize={pageSize}
-          // checkboxSelection
-          // disableSelectionOnClick
           experimentalFeatures={{ newEditingApi: true }}
           onPageSizeChange={(newPageSize) => setPageSize(newPageSize)}
           rowsPerPageOptions={[5, 10, 20]}
           onRowClick={(row, e) => handleRowClick(row, e)}
+          getRowId={(row) => row?.clientId}
           pagination
         />
       </Box>
--- src/pages/viewClients/ViewClients.jsx
+++ src/pages/viewClients/ViewClients.jsx
@@ -1,49 +1,114 @@
-import { FormControl, InputLabel, MenuItem, Select } from '@mui/material'
-import React, { useState } from 'react'
+import { TextField } from '@mui/material'
+import React, { useEffect, useState } from 'react'
 import Sidebar from '../../components/sidebar/Sidebar'
 import Topber from '../../components/topbar/Topber'
 import './viewClients.scss'
-import searchImg from '../../utils/images/searchImg.png'
 import ViewClientsDatagrid from '../../components/viewClientsDatagrid/ViewClientsDatagrid'
+import Loading from '../../components/loading/Loading'
+import ErrorComponent from '../../components/error/Error'
+import { useSelector } from 'react-redux'
+import { publicRequest } from '../../functions/requestMethods'
 
-const ViewClients = (props) => {
-  const [searched, setSearched] = useState(false)
-  const loggedInUserRole = props.userDetails?.role
+const ViewClients = () => {
+  // LOGGED IN USER TOKEN
+  const { token } = useSelector((state) => state?.user?.currentUser?.data)
+
+  // GET CURRENT LOGGED IN USER
+  const { currentUser } = useSelector((state) => state?.user)
+  const loggedInUserRole = currentUser?.data?.role
+  const userName = currentUser?.data?.profile?.fullName
+
+  // TABLE DATA
+  const [tableData, setTableData] = useState([])
+  const [searchedTableData, setSearchedTableData] = useState([])
+
+  // LOADING AND ERROR DATA
+  const [loadingClients, setLoadingClients] = useState(false)
+  const [errorLoadingClients, setErrorLoadingClients] = useState(false)
+  const [errorLoadingClientsMessage, setLoadingClientsErrorMessage] =
+    useState(null)
+  // END OF LOADING AND ERROR DATA
+
+  // FUNCTION TO GET AND SET PENDING CANDIDATES
+  const getAllClients = async () => {
+    try {
+      setLoadingClients(true)
+      const res = await publicRequest.get('Client/Client-list', {
+        headers: {
+          Accept: '*',
+          Authorization: `Bearer ${token}`,
+        },
+      })
+
+      if (res.data) {
+        console.log(res.data)
+        setTableData(res.data?.data === '' ? [] : res.data?.data)
+        setSearchedTableData(res.data?.data === '' ? [] : res.data?.data)
+        setLoadingClients(false)
+      } else {
+        console.log(res.data)
+      }
+    } catch (error) {
+      setLoadingClients(false)
+      setErrorLoadingClients(true)
+      setLoadingClientsErrorMessage(error)
+
+      console.log(error)
+    }
+  }
+  // END OF FUNCTION TO GET AND SET PENDING CANDIDATES
+
+  // SEARCH FUNCTIONALITY
+  const handleSearchParamsChange = (e) => {
+    let filteredPendingCandidatesArray
+    filteredPendingCandidatesArray = tableData.filter((tableDatum) =>
+      tableDatum?.candidateName?.includes(e.target.value.trim())
+    )
+    setSearchedTableData(filteredPendingCandidatesArray)
+    // console.log(filteredPendingCandidatesArray)
+  }
+  // END OF SEARCH FUNCTIONALITY
+
+  // USE EFFECT TO GET ALL CANDIDATES AS THE PAGE LOADS
+  useEffect(() => {
+    getAllClients()
+  }, [])
 
   return (
     <div className='viewClientsWrapper'>
       <Sidebar loggedInUserRole={loggedInUserRole} />
       <div className='viewClientsRight'>
-        <Topber userName={props.userDetails?.name} />
+        <Topber userName={userName} />
         <div className='viewClientsMainWrapper'>
           <div className='viewClientsMainTop'>
             <h3 className='viewClientsMainTopTitle'>Search</h3>
             <div className='viewClientsMainTopForm'>
-              <FormControl className='companySelect'>
-                <InputLabel id='demo-simple-select-label'>
-                  Company name
-                </InputLabel>
-                <Select
-                  labelId='demo-simple-select-label'
-                  id='demo-simple-select'
-                  //   value={age}
-                  label='Company name'
-                  //   onChange={handleChange}
-                >
-                  <MenuItem value={10}>Chicken Republic</MenuItem>
-                  <MenuItem value={20}>Union Bank</MenuItem>
-                </Select>
-              </FormControl>
+              <TextField
+                id='outlined-search'
+                label='Candidate name'
+                type='search'
+                className='candidateName'
+                onChange={(e) => handleSearchParamsChange(e)}
+              />
             </div>
           </div>
           <div className='viewClientsMainBottom'>
-            {searched && (
-              <>
-                <img src={searchImg} alt='Search' className='searchImg' />
-                <h3>Nothing to see here, yet</h3>
-              </>
+            {loadingClients || errorLoadingClients ? (
+              loadingClients ? (
+                <Loading />
+              ) : (
+                <ErrorComponent
+                  errorMessage={
+                    errorLoadingClientsMessage && errorLoadingClientsMessage
+                  }
+                />
+              )
+            ) : (
+              <ViewClientsDatagrid
+                userDetails={currentUser}
+                tableData={searchedTableData}
+              />
             )}
-            {<ViewClientsDatagrid userDetails={props.userDetails} />}
           </div>
         </div>
       </div>
