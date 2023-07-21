037597c90fcd9a5b9b12ec81c2f1cf714ff49278
Alausa-AbdulAzeez <alausaabdulazeez@gmail.com>
2023-07-21 23:19:00 +0100

    fixed candidate search error

--- src/pages/candidateSearch/CandidateSearch.jsx
+++ src/pages/candidateSearch/CandidateSearch.jsx
@@ -1,212 +1,212 @@
-import { Autocomplete, TextField } from '@mui/material'
-import React, { useEffect, useState } from 'react'
-import Sidebar from '../../components/sidebar/Sidebar'
+import { Autocomplete, TextField } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import Sidebar from "../../components/sidebar/Sidebar";
 
-import Topber from '../../components/topbar/Topber'
-import './candidateSearch.scss'
-import CandidateSearchDatagrid from '../../components/candidateSearchDatagrid/CandidateSearchDatagrid'
-import { useSelector } from 'react-redux'
-import { publicRequest } from '../../functions/requestMethods'
-import { ToastContainer, toast } from 'react-toastify'
-import ErrorComponent from '../../components/error/Error'
-import Loading from '../../components/loading/Loading'
-import { RxReload } from 'react-icons/rx'
-import DatePicker from 'react-datepicker'
+import Topber from "../../components/topbar/Topber";
+import "./candidateSearch.scss";
+import CandidateSearchDatagrid from "../../components/candidateSearchDatagrid/CandidateSearchDatagrid";
+import { useSelector } from "react-redux";
+import { publicRequest } from "../../functions/requestMethods";
+import { ToastContainer, toast } from "react-toastify";
+import ErrorComponent from "../../components/error/Error";
+import Loading from "../../components/loading/Loading";
+import { RxReload } from "react-icons/rx";
+import DatePicker from "react-datepicker";
 
 const CandidateSearch = () => {
   // MISCELLANEOUS
-  const toastId = React.useRef(null)
+  const toastId = React.useRef(null);
 
   // GET CURRENT LOGGED IN USER
-  const { currentUser } = useSelector((state) => state?.user)
-  const loggedInUserRole = currentUser?.data?.role
-  const userData = currentUser?.data
+  const { currentUser } = useSelector((state) => state?.user);
+  const loggedInUserRole = currentUser?.data?.role;
+  const userData = currentUser?.data;
 
   // LOGGED IN USER TOKEN
-  const { token } = useSelector((state) => state?.user?.currentUser?.data)
+  const { token } = useSelector((state) => state?.user?.currentUser?.data);
 
   // LOADING AND ERROR DATA
-  const [loading, setLoading] = useState(false)
-  const [error, setError] = useState(false)
-  const [errorMessage, setErrorMessage] = useState(null)
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState(null);
   // END OF LOADING AND ERROR DATA
 
   // TABLE DATA
-  const [getAllCandidatestableData, setTableData] = useState([])
-  const [filteredData, setFilteredData] = useState([])
+  const [tableData, setTableData] = useState([]);
+  const [filteredData, setFilteredData] = useState([]);
 
   // CLIENTS DATA
-  const [clients, setClients] = useState([])
-  const [clientId, setClientId] = useState(null)
+  const [clients, setClients] = useState([]);
+  const [clientId, setClientId] = useState(null);
 
   // CANDIDATE'S PHONE NUMBER
-  const [phoneNumber, setPhoneNumber] = useState('')
+  const [phoneNumber, setPhoneNumber] = useState("");
 
   // DATE SELECTION
-  const [startDate, setStartDate] = useState(null)
+  const [startDate, setStartDate] = useState(null);
 
   // FILTER PARAMS
   const [filters, setFilters] = useState({
-    clientId: '',
-    phoneNumberOrName: '',
-    date: '',
-  })
+    clientId: "",
+    phoneNumberOrName: "",
+    date: "",
+  });
 
   // FUNCTION TO HANDLE INPUT CHANGES
   const handleInputChange = (event, name, data) => {
-    if (name === 'phoneNumberOrName') {
-      setFilters({ ...filters, [name]: event?.target?.value?.trim() })
+    if (name === "phoneNumberOrName") {
+      setFilters({ ...filters, [name]: event?.target?.value?.trim() });
     }
-    if (name === 'date') {
-      setStartDate(data)
-      setFilters({ ...filters, [name]: data })
+    if (name === "date") {
+      setStartDate(data);
+      setFilters({ ...filters, [name]: data });
     }
-    if (name === 'clientId') {
-      setFilters({ ...filters, [name]: data?.clientId })
+    if (name === "clientId") {
+      setFilters({ ...filters, [name]: data?.clientId });
     }
     // setFilters({ ...filters, [name]: value.trim() });
-  }
+  };
   // END OF FUNCTION TO HANDLE INPUT CHANGES
 
   // FUNCTION TO GET AND SET ALL CANDIDATES
   const getAllCandidates = async () => {
-    setStartDate(null)
-    setFilters({ clientId: '', phoneNumberOrName: '', date: '' })
+    setStartDate(null);
+    setFilters({ clientId: "", phoneNumberOrName: "", date: "" });
 
     try {
-      setLoading(true)
-      const res = await publicRequest.get('/Candidate', {
+      setLoading(true);
+      const res = await publicRequest.get("/Candidate", {
         headers: {
-          Accept: '*',
+          Accept: "*",
           Authorization: `Bearer ${token}`,
         },
-      })
+      });
 
       if (res.data) {
-        setTableData(res.data?.data?.reverse())
-        console.log(res.data?.data)
-        setFilteredData(res.data?.data?.reverse())
-        setLoading(false)
+        setTableData(res.data?.data?.reverse());
+        console.log(res.data?.data);
+        setFilteredData(res.data?.data?.reverse());
+        setLoading(false);
       } else {
-        console.log(res.data)
+        console.log(res.data);
       }
     } catch (error) {
-      setLoading(false)
-      setError(true)
-      setErrorMessage(error)
+      setLoading(false);
+      setError(true);
+      setErrorMessage(error);
 
-      console.log(error)
+      console.log(error);
     }
-  }
+  };
   // END OF FUNCTION TO GET AND SET ALL CANDIDATES
 
   //  FUNCTIONALITIES FOR FETCHING AND SETTING CLIENTS
 
   const getAllClients = async () => {
     try {
-      const res = await publicRequest.get('Client/Client-list', {
+      const res = await publicRequest.get("Client/Client-list", {
         headers: {
-          Accept: '*',
+          Accept: "*",
           Authorization: `Bearer ${token}`,
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
-      })
+      });
 
       if (res.data) {
-        setClients(res.data.data)
+        setClients(res.data.data);
       } else {
-        console.log(res.data)
+        console.log(res.data);
       }
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
+  };
 
   //  END OF FUNCTIONALITIES FOR FETCHING AND SETTING CLIENTS
 
   // FUNCTION FOR SETTING CLIENT ID
   const handlescheduleCandidateInfo = (e, dataName, data) => {
-    setClientId(data?.clientId)
-  }
+    setClientId(data?.clientId);
+  };
   //END OF FUNCTION FOR SETTING CLIENT ID
 
   // USE EFFECT TO GET ALL CANDIDATES AS THE PAGE LOADS
   useEffect(() => {
-    getAllCandidates()
-  }, [])
+    getAllCandidates();
+  }, []);
 
   // use effect to call the getAllClients function as the page loads
   useEffect(() => {
-    getAllClients()
-  }, [])
+    getAllClients();
+  }, []);
   // end of use effect to call the getAllClients function as the page loads
 
   // FUNCTION TO HANDLE PHONE NUMBER CHANGE
   const handlePhoneNumberChange = (e) => {
-    setPhoneNumber(e.target?.value)
-  }
+    setPhoneNumber(e.target?.value);
+  };
   // END OF FUNCTION TO HANDLE PHONE NUMBER CHANGE
 
   // FUNCTION TO HANDLE CANDIDATE SEARCH
   const handleCandidateSearch = async () => {
-    toastId.current = toast('Please wait...', {
+    toastId.current = toast("Please wait...", {
       autoClose: 3000,
       isLoading: true,
-    })
+    });
     try {
       const res = await publicRequest.get(
         `Candidate/SearchByPhoneNumber?Clientid=${clientId}&phone=${phoneNumber?.trim()}`,
         {
           headers: {
-            Accept: '*',
+            Accept: "*",
             Authorization: `Bearer ${token}`,
           },
         }
-      )
+      );
 
       if (res?.data?.data?.length === 0) {
-        throw new Error('Candidate not found')
+        throw new Error("Candidate not found");
       } else {
         toast.update(toastId.current, {
-          render: 'Candidate found!',
-          type: 'success',
+          render: "Candidate found!",
+          type: "success",
           isLoading: false,
           autoClose: 3000,
-        })
-        setTableData(res?.data?.data)
+        });
+        setTableData(res?.data?.data);
       }
     } catch (error) {
-      console.log(error.message)
+      console.log(error.message);
       toast.update(toastId.current, {
-        type: 'error',
+        type: "error",
         autoClose: 3000,
         isLoading: false,
         render: `${
           error?.response?.data?.title ||
           error?.response?.data?.description ||
           error?.message ||
-          'Something went wrong, please try again'
+          "Something went wrong, please try again"
         }`,
-      })
+      });
     }
-  }
+  };
   // END FUNCTION TO HANDLE CANDIDATE SEARCH
 
   // FUNCTION TO FILTER DATA
   const filterData = () => {
-    console.log(tableData)
+    // console.log(tableData)
     const filteredData = tableData.filter((item) => {
-      const { clientId, phoneNumberOrName, date } = filters
-      console.log(clientId, phoneNumberOrName, date)
+      const { clientId, phoneNumberOrName, date } = filters;
+      console.log(clientId, phoneNumberOrName, date);
 
-      const correctDate = new Date(date)
+      const correctDate = new Date(date);
 
-      console.log(correctDate.toLocaleString())
-      const month = (date && date?.getMonth() + 1).toString().padStart(2, '0')
+      console.log(correctDate.toLocaleString());
+      const month = (date && date?.getMonth() + 1).toString().padStart(2, "0");
 
-      const year = date && date?.getFullYear()
+      const year = date && date?.getFullYear();
 
-      const newString = year && month ? year + '-' + month : ''
+      const newString = year && month ? year + "-" + month : "";
 
       // const newString =
       //   correctDate.toLocaleString().split(",")[0].split("/")[2] +
@@ -214,34 +214,34 @@ const CandidateSearch = () => {
       //   "0" +
       //   correctDate.toLocaleString().split(",")[0].split("/")[0];
 
-      const itemCompanyId = item?.clientid?.toString().includes(clientId)
+      const itemCompanyId = item?.clientid?.toString().includes(clientId);
       const itemPhoneNumber = item?.phoneNumber
         ?.toString()
-        .includes(phoneNumberOrName)
+        .includes(phoneNumberOrName);
       const itemName = item?.candidateName
         ?.toLowerCase()
-        .includes(phoneNumberOrName.toLowerCase())
-      const itemDate = item?.createdDate?.substring(0, 7).includes(newString)
-      console.log(itemDate)
+        .includes(phoneNumberOrName.toLowerCase());
+      const itemDate = item?.createdDate?.substring(0, 7).includes(newString);
+      console.log(itemDate);
       return (
-        (clientId === '' || itemCompanyId) &&
-        (phoneNumberOrName === '' || itemPhoneNumber || itemName) &&
-        (date === '' || itemDate)
-      )
-    })
-    console.log(filteredData)
-    setFilteredData(filteredData)
-  }
+        (clientId === "" || itemCompanyId) &&
+        (phoneNumberOrName === "" || itemPhoneNumber || itemName) &&
+        (date === "" || itemDate)
+      );
+    });
+    console.log(filteredData);
+    setFilteredData(filteredData);
+  };
   // END OF FUNCTION TO FILTER DATA
 
   return (
     <>
       <ToastContainer />
-      <div className='candidateSearchWrapper'>
+      <div className="candidateSearchWrapper">
         <Sidebar loggedInUserRole={loggedInUserRole} />
-        <div className='candidateSearchRight'>
+        <div className="candidateSearchRight">
           <Topber userData={userData} />
-          <div className='candidateSearchMainWrapper'>
+          <div className="candidateSearchMainWrapper">
             {/* <div className='candidateSearchMainTop'>
               <h3 className='candidateSearchMainTopTitle'>Search</h3>
               <div className='candidateSearchMainTopForm'>
@@ -283,7 +283,7 @@ const CandidateSearch = () => {
                 </button>
               </div>
             </div> */}
-            <div className='filterContainer'>
+            <div className="filterContainer">
               <Autocomplete
                 // disablePortal
                 options={clients}
@@ -291,45 +291,45 @@ const CandidateSearch = () => {
                   `${option.clientName} ${option.email}`
                 }
                 onChange={(e, option) =>
-                  handleInputChange(e, 'clientId', option)
+                  handleInputChange(e, "clientId", option)
                 }
                 key={loading}
                 sx={{ width: 200 }}
                 renderInput={(params) => (
-                  <TextField {...params} label='Client Name' size='small' />
+                  <TextField {...params} label="Client Name" size="small" />
                 )}
               />
               <TextField
-                id='outlined-search'
+                id="outlined-search"
                 label="Candidate's Name/PhoneNo"
-                type='search'
-                className='candidateSearchName'
-                onChange={(e) => handleInputChange(e, 'phoneNumberOrName')}
-                size='small'
+                type="search"
+                className="candidateSearchName"
+                onChange={(e) => handleInputChange(e, "phoneNumberOrName")}
+                size="small"
                 value={filters?.phoneNumberOrName}
               />
-              <div className='filterDateWrapper'>
+              <div className="filterDateWrapper">
                 <DatePicker
-                  placeholderText='Select a date'
-                  onChange={(date, e) => handleInputChange(e, 'date', date)}
-                  dateFormat='MM/yyyy'
+                  placeholderText="Select a date"
+                  onChange={(date, e) => handleInputChange(e, "date", date)}
+                  dateFormat="MM/yyyy"
                   showMonthYearPicker
-                  className='filterDate'
+                  className="filterDate"
                   selected={startDate}
                 />
               </div>
-              <button className='searchFilterBtn' onClick={filterData}>
+              <button className="searchFilterBtn" onClick={filterData}>
                 Search
               </button>
               <button
-                className='resetBtn'
+                className="resetBtn"
                 onClick={getAllCandidates}
                 // onClick={() => window.location.reload()}
               >
                 Reset
               </button>
             </div>
-            <div className='candidateSearchMainBottom'>
+            <div className="candidateSearchMainBottom">
               {/* <Loading /> */}
               {loading || error ? (
                 loading ? (
@@ -349,7 +349,7 @@ const CandidateSearch = () => {
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default CandidateSearch
+export default CandidateSearch;
