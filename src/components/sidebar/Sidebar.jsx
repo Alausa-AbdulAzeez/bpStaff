4516d5726fb506963f1d590fa048c64ba8ddde02
Alausa-AbdulAzeez <alausaabdulazeez@gmail.com>
2023-08-02 21:11:36 +0100

    made sidebar toggle functionality

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,12 +1,12 @@
-import React, { useEffect } from 'react'
-import './sidebar.scss'
+import React, { useEffect } from "react";
+import "./sidebar.scss";
 
-import LogoImg from '../../utils/images/sidebarBiopath2.png'
-import { BsFillPersonFill } from 'react-icons/bs'
+import LogoImg from "../../utils/images/sidebarBiopath2.png";
+import { BsFillPersonFill } from "react-icons/bs";
 
-import { FiLogOut } from 'react-icons/fi'
-import { NavLink } from 'react-router-dom'
-import AlertDialogSlide from '../Dialogue'
+import { FiLogOut } from "react-icons/fi";
+import { NavLink } from "react-router-dom";
+import AlertDialogSlide from "../Dialogue";
 import {
   customerCareData,
   generalData,
@@ -18,31 +18,31 @@ import {
   receptionistData,
   reportOfficerData,
   roleSpecificData,
-} from '../../utils/data/sidebarData'
-import { useSelector } from 'react-redux'
-import { TbReportAnalytics } from 'react-icons/tb'
+} from "../../utils/data/sidebarData";
+import { useSelector } from "react-redux";
+import { IoIosMenu } from "react-icons/io";
 
 const Sidebar = () => {
   // LOGOUT DIALOGUE
-  const [open, setOpen] = React.useState(false)
-  const [sidebarList, setSidebarList] = React.useState(generalList)
+  const [open, setOpen] = React.useState(false);
+  const [sidebarList, setSidebarList] = React.useState(generalList);
   const sidebarListNoPendingCandidate = sidebarList.filter(
-    (sidebarItem) => sidebarItem?.title !== 'Pending Candidates'
-  )
+    (sidebarItem) => sidebarItem?.title !== "Pending Candidates"
+  );
   const sidebarListNoViewClients = sidebarList.filter(
     (sidebarItem) =>
-      sidebarItem?.title !== 'View Clients' &&
-      sidebarItem?.title !== 'Pending Candidates' &&
-      sidebarItem?.title !== 'Candidate Search'
-  )
-  const sidebarData = generalData
+      sidebarItem?.title !== "View Clients" &&
+      sidebarItem?.title !== "Pending Candidates" &&
+      sidebarItem?.title !== "Candidate Search"
+  );
+  const sidebarData = generalData;
 
   // GET CURRENT LOGGED IN USER
-  const { currentUser } = useSelector((state) => state?.user)
-  const loggedInUserRole = currentUser?.data?.role?.[0]
+  const { currentUser } = useSelector((state) => state?.user);
+  const loggedInUserRole = currentUser?.data?.role?.[0];
   // const loggedInUserRole = 'partnerLabManager'
 
-  let sidebarInfo
+  let sidebarInfo;
 
   // FUNCTION TO SET DATA
   // const setData = () => {
@@ -60,140 +60,151 @@ const Sidebar = () => {
   // INITIALIZE SIDEBAR DATA
 
   switch (loggedInUserRole) {
-    case 'Reception':
-      sidebarInfo = [...receptionistData, ...sidebarList]
-      break
-    case 'Phlebotomy':
-      sidebarInfo = [...sidebarList, ...phlebotomistData]
+    case "Reception":
+      sidebarInfo = [...receptionistData, ...sidebarList];
+      break;
+    case "Phlebotomy":
+      sidebarInfo = [...sidebarList, ...phlebotomistData];
 
-      break
-    case 'MainLab1':
-      sidebarInfo = [...sidebarList, ...labScientistData]
+      break;
+    case "MainLab1":
+      sidebarInfo = [...sidebarList, ...labScientistData];
 
-      break
-    case 'Quality assurance':
-      sidebarInfo = [...sidebarList, ...qualityAssuranceData]
+      break;
+    case "Quality assurance":
+      sidebarInfo = [...sidebarList, ...qualityAssuranceData];
 
-      break
-    case 'Report':
-      sidebarInfo = [...sidebarList, ...reportOfficerData]
-      break
-    case 'PartnerLab Manager':
-      sidebarInfo = [...partnerLabManagerData, ...sidebarListNoPendingCandidate]
-      break
-    case 'Customer Care':
-      sidebarInfo = [...customerCareData, ...sidebarListNoViewClients]
-      break
+      break;
+    case "Report":
+      sidebarInfo = [...sidebarList, ...reportOfficerData];
+      break;
+    case "PartnerLab Manager":
+      sidebarInfo = [
+        ...partnerLabManagerData,
+        ...sidebarListNoPendingCandidate,
+      ];
+      break;
+    case "Customer Care":
+      sidebarInfo = [...customerCareData, ...sidebarListNoViewClients];
+      break;
 
     default:
-      break
+      break;
   }
 
   const handleClickOpen = () => {
-    setOpen(true)
-  }
+    setOpen(true);
+  };
 
   const handleClose = () => {
-    setOpen(false)
-  }
+    setOpen(false);
+  };
 
   return (
-    <div className='sidebarWrapper'>
-      <AlertDialogSlide
-        open={open}
-        handleClose={handleClose}
-        message=' Are you sure you want to logout?'
-        link='/login'
-        title='Logout'
-      />
-      <div className='sidebarTop'>
-        <div className='sidebarTopImageWrapper'>
-          <img src={LogoImg} alt='Logo' /> <span>Biopath MedLab</span>
-        </div>
+    <>
+      <div className="menuIconWrapper">
+        <IoIosMenu className="menuIcon" />
       </div>
-      <div className='sidebarBottom'>
-        <div className='sidebarBottomTop'>
-          {sidebarData?.map((singleItem, index) => {
-            return (
-              <ul className='ulTitle' key={index}>
-                {singleItem?.ulTitle}
-                {singleItem?.listItems?.map((listItem, index) => {
-                  return (
-                    <NavLink
-                      to={listItem.link}
-                      style={{ textDecoration: 'none' }}
-                      key={index}
-                    >
-                      {({ isActive }) => (
-                        <li
-                          className={
-                            isActive
-                              ? 'activeLink sidebarListItem'
-                              : 'sidebarListItem'
-                          }
-                        >
-                          {listItem.icon}
-                          <span> {listItem.title}</span>
-                        </li>
-                      )}
-                    </NavLink>
-                  )
-                })}
-              </ul>
-            )
-          })}
-        </div>
-        <div className='sidebarBottomTopBottom'>
-          <ul className='ulTitle'>
-            LIST
-            {sidebarInfo?.map((listItem, index) => {
-              return (
-                <NavLink
-                  to={listItem.link}
-                  style={{ textDecoration: 'none' }}
-                  key={index}
-                >
-                  {({ isActive }) => (
-                    <li
-                      className={
-                        isActive
-                          ? 'activeLink sidebarListItem'
-                          : 'sidebarListItem'
-                      }
-                    >
-                      {listItem.icon}
-                      <span> {listItem.title}</span>
-                    </li>
-                  )}
-                </NavLink>
-              )
-            })}
-          </ul>
-        </div>
-        <div className='sidebarBottomBottom'>
-          <ul className='ulTitle'>
-            USER
-            <NavLink to='/profile' style={{ textDecoration: 'none' }}>
-              {({ isActive }) => (
-                <li
-                  className={
-                    isActive ? 'activeLink sidebarListItem' : 'sidebarListItem'
-                  }
-                >
-                  <BsFillPersonFill className='sidebarIcon' />
-                  <span> Profile</span>
-                </li>
-              )}
-            </NavLink>
-            <li className='sidebarListItem' onClick={handleClickOpen}>
-              <FiLogOut className='sidebarIcon' />
-              <span> Logout</span>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </div>
-  )
-}
+      <div className={`sidebarWrapper ${open ? "open" : ""}`}>
+        <AlertDialogSlide
+          open={open}
+          handleClose={handleClose}
+          message=" Are you sure you want to logout?"
+          link="/login"
+          title="Logout"
+        />
 
-export default Sidebar
+        <div className="sidebarTop">
+          <div className="sidebarTopImageWrapper">
+            <img src={LogoImg} alt="Logo" /> <span>Biopath MedLab</span>
+          </div>
+        </div>
+        <div className="sidebarBottom">
+          <div className="sidebarBottomTop">
+            {sidebarData?.map((singleItem, index) => {
+              return (
+                <ul className="ulTitle" key={index}>
+                  {singleItem?.ulTitle}
+                  {singleItem?.listItems?.map((listItem, index) => {
+                    return (
+                      <NavLink
+                        to={listItem.link}
+                        style={{ textDecoration: "none" }}
+                        key={index}
+                      >
+                        {({ isActive }) => (
+                          <li
+                            className={
+                              isActive
+                                ? "activeLink sidebarListItem"
+                                : "sidebarListItem"
+                            }
+                          >
+                            {listItem.icon}
+                            <span> {listItem.title}</span>
+                          </li>
+                        )}
+                      </NavLink>
+                    );
+                  })}
+                </ul>
+              );
+            })}
+          </div>
+          <div className="sidebarBottomTopBottom">
+            <ul className="ulTitle">
+              LIST
+              {sidebarInfo?.map((listItem, index) => {
+                return (
+                  <NavLink
+                    to={listItem.link}
+                    style={{ textDecoration: "none" }}
+                    key={index}
+                  >
+                    {({ isActive }) => (
+                      <li
+                        className={
+                          isActive
+                            ? "activeLink sidebarListItem"
+                            : "sidebarListItem"
+                        }
+                      >
+                        {listItem.icon}
+                        <span> {listItem.title}</span>
+                      </li>
+                    )}
+                  </NavLink>
+                );
+              })}
+            </ul>
+          </div>
+          <div className="sidebarBottomBottom">
+            <ul className="ulTitle">
+              USER
+              <NavLink to="/profile" style={{ textDecoration: "none" }}>
+                {({ isActive }) => (
+                  <li
+                    className={
+                      isActive
+                        ? "activeLink sidebarListItem"
+                        : "sidebarListItem"
+                    }
+                  >
+                    <BsFillPersonFill className="sidebarIcon" />
+                    <span> Profile</span>
+                  </li>
+                )}
+              </NavLink>
+              <li className="sidebarListItem" onClick={handleClickOpen}>
+                <FiLogOut className="sidebarIcon" />
+                <span> Logout</span>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Sidebar;
